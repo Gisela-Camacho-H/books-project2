1c4cd2c868f13f5055ac04aa0c7620bcb9a3d4ec
Cinthia Gisela Camacho <gis25cam@gmail.com>
2023-02-18 00:14:04 -0600

    add OAuth to app.js

--- src/controllers/profile.js
+++ src/controllers/profile.js
@@ -1,9 +1,8 @@
 const {requiresAuth} = require('express-openid-connect');
 
-const getProfile = async (req, res) => {
+var getProfile = (req, res) => {
     res.send(JSON.stringify(req.oidc.user, null, 2));
   }
-
 
 module.exports = {
     requiresAuth,
