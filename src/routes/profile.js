cbda5d10c6b5148b3805d12483aa8c99f630b31c
Cinthia Gisela Camacho <gis25cam@gmail.com>
2023-02-18 00:19:47 -0600

    change profile route

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const profileController = require('../controllers/profile');
 
-router.get('/profile', profileController.requiresAuth(), profileController.getProfile);
+router.get('/', profileController.requiresAuth(), profileController.getProfile);
 
 module.exports = router;
