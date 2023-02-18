4dd519eba8ebec82cdd7213a7011fc16fe0472b1
Cinthia Gisela Camacho <gis25cam@gmail.com>
2023-02-17 19:18:47 -0600

    add OAuth code

--- src/swagger.js
+++ src/swagger.js
@@ -5,10 +5,10 @@ const doc = {
     title: 'My API',
     description: 'Description',
   },
-  host: 'localhost:8080',
-  schemes: ['http'],
-  //host: 'books-project2.onrender.com',
-  //schemes: ['https'],
+  //host: 'localhost:8080',
+  //schemes: ['http'],
+  host: 'books-project2.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
